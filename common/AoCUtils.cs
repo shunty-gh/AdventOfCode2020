2625b1430c9a7d64808d8a45ee594792070166b6
Steven Hunt <sph@shunty.co.uk>
2020-11-27 14:14:34 +0000

    Base updates

---
+++ common/AoCUtils.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Shunty.AdventOfCode2020
+{
+    public static class AoCUtils
+    {
+        /// <summary>
+        /// Find the file containing the text input for the given day
+        /// </summary>
+        /// <param name="day">The day number</param>
+        /// <param name="part">An optional part number eg for the second part of the problem</param>
+        /// <returns>A string containing the path name of the file, if found</returns>
+        public static string FindInput(int day, int part = 0)
+        {
+            var parttxt = part > 0 ? $"-part{part}" : "";
+            var fname = $"day{day:D2}{parttxt}-input.txt";
+
+            // Look in the current directory first
+            var result = Path.Combine(".", fname);
+            if (File.Exists(result))
+                return result;
+
+            // Look in a day specific directory
+            result = Path.Combine(".", $"day{day:D2}", fname);
+            if (File.Exists(result))
+                return result;
+
+            // Otherwise return blank
+            return "";
+        }
+
+        /// <summary>
+        /// Read and return the input for the given day (and optional part) as a list of strings
+        /// </summary>
+        /// <param name="day">The day number</param>
+        /// <param name="part">An optional part number eg for the second part of the problem</param>
+        public static IList<string> GetDayLines(int day, int part = 0)
+        {
+            var fname = FindInput(day, part);
+            if (string.IsNullOrWhiteSpace(fname))
+                throw new FileNotFoundException($"Unable to find input file for day {day}{(part > 0 ? $" and part {part}" : "")}");
+
+            return File.ReadAllLines(fname)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Read and return the input for the given day (and optional part) as a single string
+        /// </summary>
+        /// <param name="day">The day number</param>
+        /// <param name="part">An optional part number eg for the second part of the problem</param>
+        public static string GetDayText(int day, int part = 0)
+        {
+            var fname = FindInput(day, part);
+            if (string.IsNullOrWhiteSpace(fname))
+                throw new FileNotFoundException($"Unable to find input file for day {day}{(part > 0 ? $" and part {part}" : "")}");
+
+            return File.ReadAllText(fname);
+        }
+    }
+}
---
+++ day-index.md
@@ -0,0 +1,18 @@
+# Advent Of Code 2020
+
+## Notes for each day
+So that I can find specific types of approach if I ever need them again.
+
+### Helpful Links And Notes
+* T.B.A
+
+### Other Solutions
+* T.B.A
+
+| URL                                      | Language(s) | Notes |
+|------------------------------------------|----------|-------|
+| https://github.com/... | ... | |
+
+### Day 1 -
+
+### Day 2 -
