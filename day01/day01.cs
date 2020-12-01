0b09cfb84aaa9bb52131665b5fdc028ad48052dc
Steven Hunt <sph@shunty.co.uk>
2020-12-01 17:13:34 +0000

    Add day 1

--- day01/day01.cs
+++ day01/day01.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Serilog;
 using Spectre.Console;
@@ -11,9 +12,38 @@ namespace Shunty.AdventOfCode2020
 
         public async Task Execute(ILogger logger)
         {
-            var input = AoCUtils.GetDayLines(Day);
-            var part1 = 0;
-            var part2 = 0;
+            var input = AoCUtils.GetDayLines(Day).Select(i => Int64.Parse(i)).ToList();
+            Int64 part1 = 0, part2 = 0;
+            for (var i = 0; i < input.Count; i++)
+            {
+                for (var j = 0; j < input.Count; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    if (part1 == 0)
+                    {
+                        if (input[i] + input[j] == 2020)
+                        {
+                            part1 = input[i] * input[j];
+                        }
+                    }
+
+                    if (part2 == 0)
+                    {
+                        for (var k = 0; k < input.Count; k++)
+                        {
+                            if (input[i] + input[j] + input[k] == 2020)
+                            {
+                                part2 = input[i] * input[j] * input[k];
+                            }
+                        }
+                    }
+
+                    if (part1 > 0 && part2 > 0)
+                        break;
+                }
+            }
 
             AnsiConsole.MarkupLine($"[white]Day [blue]{Day}[/]; Part [blue]1[/]:[/] [yellow]{part1}[/]");
             AnsiConsole.MarkupLine($"[white]Day [blue]{Day}[/]; Part [blue]2[/]:[/] [yellow]{part2}[/]");
