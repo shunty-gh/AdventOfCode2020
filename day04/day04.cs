05ca7fedd4f43ae0b4695516c008f17113c9fdd8
Steven Hunt <sph@shunty.co.uk>
2020-12-04 11:13:55 +0000

    Tidy up day 4 c#

--- day04/day04.cs
+++ day04/day04.cs
@@ -12,85 +12,44 @@ namespace Shunty.AdventOfCode2020
     {
         public int Day => 4;
 
+        static readonly string[] RequiredFields = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+        static readonly string[] EyeColours = new string[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+
         public async Task Execute(IConfiguration config, ILogger logger, bool useTestData)
         {
             await AoCUtils.GetDayInput(config, Day, "");
-
-            var input = useTestData
-                ? AoCUtils.GetTestLines(Day)
-                : AoCUtils.GetDayLines(Day);
+            var input = AoCUtils.GetDayLines(Day);
             if (!input.Any())
             {
                 AnsiConsole.MarkupLine($"[red]Error Day [blue]{Day}[/]; Part [blue]1[/]: No {(useTestData ? "test " : "")}input available[/]");
             }
 
             int part1 = 0, part2 = 0;
-            string byr = "", iyr = "", eyr = "",hgt = "",hcl = "",ecl = "",pid = "";
-            bool hasbyr = false, hasiyr = false, haseyr = false, hashgt = false, hashcl = false, hasecl = false, haspid = false;
-            for (var i = 0; i < input.Count; i++)
+            var current = new Dictionary<string, string>();
+            int lineindex = 0, maxindex = input.Count;
+            while (lineindex < maxindex)
             {
-                var line = input[i].Trim();
-                if (string.IsNullOrWhiteSpace(line))
-                {
+                var line = input[lineindex++];
 
-                    if (hasbyr && hasiyr && haseyr && hashgt && hashcl && hasecl && haspid)
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var parts = line.Split(' ');
+                    foreach (var part in parts)
+                    {
+                        var field = part.Split(':');
+                        current[field[0]] = field[1];
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(line) || lineindex >= maxindex)
+                {
+                    if (RequiredFields.All(f => current.ContainsKey(f)))
                     {
                         part1++;
-                        part2 += AreAllValid(byr, iyr, eyr, hgt, hcl, ecl, pid) ? 1 : 0;
+                        part2 += AreAllValid(current) ? 1 : 0;
                     }
-                    // Reset
-                    hasbyr = false;
-                    hasiyr = false;
-                    haseyr = false;
-                    hashgt = false;
-                    hashcl = false;
-                    hasecl = false;
-                    haspid = false;
-                    continue;
+                    current.Clear();
                 }
-
-                if (line.Contains("byr"))
-                {
-                    hasbyr = true;
-                    byr = GetFieldValue(line, "byr");
-                }
-                if (line.Contains("iyr"))
-                {
-                    hasiyr = true;
-                    iyr = GetFieldValue(line, "iyr");
-                }
-                if (line.Contains("eyr"))
-                {
-                    haseyr = true;
-                    eyr = GetFieldValue(line, "eyr");
-                }
-                if (line.Contains("hgt"))
-                {
-                    hashgt = true;
-                    hgt = GetFieldValue(line, "hgt");
-                }
-                if (line.Contains("hcl"))
-                {
-                    hashcl = true;
-                    hcl = GetFieldValue(line, "hcl");
-                }
-                if (line.Contains("ecl"))
-                {
-                    hasecl = true;
-                    ecl = GetFieldValue(line, "ecl");
-                }
-                if (line.Contains("pid"))
-                {
-                    haspid = true;
-                    pid = GetFieldValue(line, "pid");
-                }
-                // Ignore cid field
-            }
-            // Check the last line too
-            if (hasbyr && hasiyr && haseyr && hashgt && hashcl && hasecl && haspid)
-            {
-                part1++;
-                part2 += AreAllValid(byr, iyr, eyr, hgt, hcl, ecl, pid) ? 1 : 0;
             }
 
             AnsiConsole.MarkupLine($"[white]Day [blue]{Day}[/]; Part [blue]1[/]:[/] [yellow]{part1}[/]");
@@ -99,47 +58,51 @@ namespace Shunty.AdventOfCode2020
             await Task.CompletedTask;
         }
 
-        private string GetFieldValue(string line, string fieldId)
+        private bool AreAllValid(Dictionary<string, string> fields)
         {
-            if (string.IsNullOrWhiteSpace(line))
-                return "";
-
-            var start = line.IndexOf(fieldId);
-            if (start < 0)
-                return "";
-            start += (fieldId.Length + 1); // Skip the field and the trailing ':'
-
-            // Return up to the next ' ' or to the end of the line
-            var nextsp = line.IndexOf(" ", start);
-            if (nextsp < 0)
-                return line.Substring(start);
-            return line.Substring(start, nextsp - start);
-        }
-
-        private bool AreAllValid(string byr, string iyr, string eyr, string hgt, string hcl, string ecl, string pid)
-        {
-            // Check rules for each element
-            bool isvalid = true;
-            isvalid &= (int.Parse(byr) >= 1920 && int.Parse(byr) <= 2002);
-            isvalid &= (int.Parse(iyr) >= 2010 && int.Parse(iyr) <= 2020);
-            isvalid &= (int.Parse(eyr) >= 2020 && int.Parse(eyr) <= 2030);
-            if (hgt.EndsWith("cm") || hgt.EndsWith("in"))
+            // Check rules for each element. Assume that all required fields are supplied.
+            int i;
+            var result = true;
+            foreach ((var key, var v) in fields)
             {
-                var units = hgt.Substring(hgt.Length - 2);
-                var value = int.Parse(hgt.Substring(0, hgt.Length - 2));
-                isvalid &= units == "cm"
-                    ? (value >= 150 && value <= 193)
-                    : (value >= 59 && value <= 76);
+                switch (key)
+                {
+                    case "byr":
+                        result &= (int.TryParse(v, out i) && i >= 1920 && i <= 2002);
+                        break;
+                    case "iyr":
+                        result &= (int.TryParse(v, out i) && i >= 2010 && i <= 2020);
+                        break;
+                    case "eyr":
+                        result &= (int.TryParse(v, out i) && i >= 2020 && i <= 2030);
+                        break;
+                    case "hgt":
+                        result &= (v.EndsWith("cm") || v.EndsWith("in"));
+                        if (result)
+                        {
+                            var units = v.Substring(v.Length - 2);
+                            var value = int.Parse(v.Substring(0, v.Length - 2));
+                            result &= units == "cm"
+                                ? (value >= 150 && value <= 193)
+                                : (value >= 59 && value <= 76);
+                        }
+                        break;
+                    case "hcl":
+                        result &= (v.StartsWith("#")
+                            && (v.Length == 7)
+                            && v.Substring(1)
+                                .All(c => Uri.IsHexDigit(c))
+                            );
+                        break;
+                    case "ecl":
+                        result &= EyeColours.Contains(v);
+                        break;
+                    case "pid":
+                        result &= ((v.Length == 9) && v.All(c => Char.IsDigit(c)));
+                        break;
+                }
             }
-            else
-            {
-                isvalid = false;
-            }
-            isvalid &= (hcl.StartsWith("#") && (hcl.Length == 7) && hcl.Substring(1).All(c => Char.IsDigit(c) || (c >= 'a' && c <= 'f')));
-            isvalid &= ((new string[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}).Contains(ecl));
-            isvalid &= ((pid.Length == 9) && pid.All(c => Char.IsDigit(c)));
-
-            return isvalid;
+            return result;
         }
     }
 }
