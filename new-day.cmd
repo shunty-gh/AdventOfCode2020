b92ff89a831c4f1aa337d4d148cd1bc15d6bb52a
Steven Hunt <sph@shunty.co.uk>
2020-12-14 14:38:51 +0000

    Update new-day batch file

--- new-day.cmd
+++ new-day.cmd
@@ -1,3 +1,17 @@
+@echo off
+if "%1x" == "x" goto Error
 mkdir day%1
 copy day-template.cs day%1\day%1.cs
 sed -i 's/Day00/Day%1/g' day%1/day%1.cs
+touch day%1/day%1-input-test.txt
+goto Done
+
+:Error
+echo Error - Day number not supplied
+echo Usage eg: new-day 12
+goto End
+
+:Done
+echo Done
+
+:End
