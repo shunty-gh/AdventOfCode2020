221f335b251784f205dc7acbe612e9f32629cc53
Steven Hunt <sph@shunty.co.uk>
2020-12-15 10:02:51 +0000

    Error checking in new-day batch file

--- new-day.cmd
+++ new-day.cmd
@@ -1,6 +1,8 @@
 @echo off
 if "%1x" == "x" goto Error
 mkdir day%1
+if %ERRORLEVEL% neq 0 goto ErrorMkdir
+
 copy day-template.cs day%1\day%1.cs
 sed -i 's/Day00/Day%1/g' day%1/day%1.cs
 touch day%1/day%1-input-test.txt
@@ -9,6 +11,10 @@ goto Done
 :Error
 echo Error - Day number not supplied
 echo Usage eg: new-day 12
+goto End
+
+:ErrorMkdir
+echo There was an error creating the directory (maybe it already exists?)
 goto End
 
 :Done
