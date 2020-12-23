8e90d2698f06796e0bf760cfce4835d46ca468b8
Steven Hunt <sph@shunty.co.uk>
2020-12-23 14:35:21 +0000

    Day 23 a bit quicker

--- day23/day23.cs
+++ day23/day23.cs
@@ -11,7 +11,6 @@ namespace Shunty.AdventOfCode2020
     {
         public class Node
         {
-            public Node Previous { get; set; }
             public Node Next { get; set; }
             public int Value { get; set; }
         }
@@ -28,11 +27,9 @@ namespace Shunty.AdventOfCode2020
             DoMoves(nodes, head, 100, 9);
 
             var part1 = "";
-            var nd = nodes[1].Next;
-            for (var i = 1; i < 9; i++)
+            for (var nd = nodes[1].Next; nd.Value != 1; nd = nd.Next)
             {
                 part1 += nd.Value;
-                nd = nd.Next;
             }
             ShowResult(1, part1);
 
@@ -40,80 +37,61 @@ namespace Shunty.AdventOfCode2020
             (nodes, head) = BuildNodes(input, 1_000_000);
             DoMoves(nodes, head, 10_000_000, 1_000_000);
 
-            nd = nodes[1].Next;
-            var part2 = (long)nd.Value * (long)nd.Next.Value;
+            var part2 = (long)nodes[1].Next.Value * (long)nodes[1].Next.Next.Value;
             ShowResult(2, part2);
         }
 
         private (Node[], Node) BuildNodes(int[] input, int maxValue)
         {
             var nodes = new Node[maxValue + 1];
-            var head = new Node { Value = input.First() };
+            var head = new Node { Value = input[0] };
             nodes[head.Value] = head;
             var prev = head;
+
             for (var i = 1; i < input.Length; i++)
             {
-                var nd = new Node { Previous = prev, Value = input[i] };
+                var nd = new Node { Value = input[i] };
                 nodes[input[i]] = nd;
                 prev.Next = nd;
                 prev = nd;
             }
 
             // For part 2 we need to add many extra nodes
-            var imax = input.Max();
-            if (maxValue > imax)
+            for (var i = input.Length + 1; i <= maxValue; i++)
             {
-                for (var i = imax + 1; i <= maxValue; i++)
-                {
-                    var nd = new Node { Previous = prev, Value = i };
-                    nodes[i] = nd;
-                    prev.Next = nd;
-                    prev = nd;
-                }
+                var nd = new Node { Value = i };
+                nodes[i] = nd;
+                prev.Next = nd;
+                prev = nd;
             }
-
             // Link the head and tail together
             prev.Next = head;
-            head.Previous = prev;
 
             return (nodes, head);
         }
 
         private void DoMoves(Node[] nodes, Node head, int moves, int maxValue)
         {
-            var next3Nodes = new Node[3];
-            var next3Values = new int[3];
-
+            var curr = head;
             for (var move = 1; move <= moves; move++)
             {
                 // Cut out next three
-                var n = head.Next;
-                for (var i = 0; i < 3; i++)
-                {
-                    next3Nodes[i] = n;
-                    next3Values[i] = n.Value;
-                    n = n.Next;
-                }
-                head.Next = n;
-                n.Previous = head;
+                var cut = curr.Next;
+                curr.Next = cut.Next.Next.Next;
                 // Find suitable destination value
-                var dest = head.Value > 1 ? head.Value - 1 : maxValue;
-                while (next3Values.Contains(dest))
+                var dest = curr.Value > 1 ? curr.Value - 1 : maxValue;
+                while (dest == cut.Value || dest == cut.Next.Value || dest == cut.Next.Next.Value)
                 {
-                    dest--;
-                    if (dest <= 0)
-                        dest = maxValue;
+                    // Positions and elements are 1 based
+                    dest = ((dest + maxValue - 1 - 1) % maxValue) + 1; // Add maxValue 'cos C# treats (-1 % N) as -1
                 }
                 // Get the node containing destination
-                n = nodes[dest];
+                var n = nodes[dest];
                 // Put the cut nodes back in
-                var tmp = n.Next;
-                n.Next = next3Nodes[0];
-                next3Nodes[0].Previous = n;
-                tmp.Previous = next3Nodes[2];
-                next3Nodes[2].Next = tmp;
-                // Move the head along
-                head = head.Next;
+                cut.Next.Next.Next = n.Next;
+                n.Next = cut;
+                // Move along
+                curr = curr.Next;
                 //Console.WriteLine(string.Join(", ", a1) + $"; Curr: {a1[ci]}");
             }
         }
